322f2ec6aeff7a5479857e2b521ee18ab6828396
hari <hareshwarmali20@gmail.com>
2023-11-21 21:39:51 +0530

    modify code

--- _1_prime_number.py
+++ _1_prime_number.py
@@ -2,15 +2,13 @@
 
 # To take input from the user
 num = int(input("Enter a number: "))
-
-
+flag = False
 if num > 1:
-
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           #print(i,"times",num//i,"is",num)
-           break
-   else:
-       print(num,"is a prime number")
-     
+    for i in range(2, num):
+        if num % i == 0:
+            flag = True
+            break
+if flag:
+    print(num, 'Number is not a prime')
+else:
+    print(num, 'Number is a prime')
