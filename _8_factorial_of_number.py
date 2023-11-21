3f02d14fbe4aa8422f5f51dfd13376fae8240e43
hari <hareshwarmali20@gmail.com>
2023-11-21 22:22:56 +0530

    use recursion

--- _8_factorial_of_number.py
+++ _8_factorial_of_number.py
@@ -2,9 +2,21 @@
 # Factorial of a number
 no = int(input("Enter the input: "))
 
+
+def recur_factorial(n):
+    if n == 1:
+        return n
+    else:
+        return n * recur_factorial(n - 1)
+
 fact = 1
 
 while no > 0:
     fact = fact * no
     no = no - 1
 print(fact)
+
+for i in range(1, no+1):
+    fact = fact * i
+
+print('factorial is ', fact)
