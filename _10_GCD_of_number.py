ad3a6e345daf1ab19a44a54efee6e59c2b35cd33
hari <hareshwarmali20@gmail.com>
2023-11-26 17:01:55 +0530

    gcd number

--- _10_GCD_of_number.py
+++ _10_GCD_of_number.py
@@ -2,14 +2,14 @@
 
 num1 = int(input("Enter the first number  :"))
 num2 = int(input("Enter the second number  :"))
-
+GCD = 1
 if num2 > num1:
-    min = num1
+    minimum = num1
 else:
-    min = num2
+    minimum = num2
 
-for i in range(1,min+1):
-    if num1%i==0 and num2%i==0:
+for i in range(1,minimum+1):
+    if num1 % i==0 and num2 % i==0:
         GCD = i
 
 print("GCD of given two numbers is : ",GCD)
