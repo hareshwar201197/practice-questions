4be5d30def681e2daaa1448e38688e8c5d6eb964
hari <hareshwarmali20@gmail.com>
2024-01-01 23:05:08 +0530

    2 pattern

---
+++ pattern2.py
@@ -0,0 +1,11 @@
+# *
+# * * *
+# * * * * *
+
+num = int(input("Enter the number of rows : "))
+k = 1
+for i in range(1, num+1):
+    for j in range(1, k+1):
+        print("1", end=" ")
+    k = k+2
+    print()
